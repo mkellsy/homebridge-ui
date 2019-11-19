436fb3986ff56b05d67e6ceef3126049fed653ca
Michael Kellsy <mkellsy@gmail.com>
2019-11-19 09:45:33 -0700

    upgrade to node 12.13.1

--- install.sh
+++ install.sh
@@ -6,7 +6,32 @@ arch=$(uname -m)
 node=$(node -v)
 node=${node#"v"}
 
-echo "Current Node Version $node"
+spin()
+{
+    spinner="⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏"
+
+    while :
+    do
+        for i in `seq 0 7`
+        do
+            printf "\r\e[93m${spinner:$i:1}\e[0m "
+            sleep 0.2
+        done
+    done
+}
+
+echo ""
+echo "Thank You for choosing HOOBS"
+echo "---------------------------------------------------------"
+
+spin &
+marker=$!
+
+trap "kill -9 $marker" `seq 0 15`
+
+sleep 0.2
+
+echo "Node Version $node"
 
 npm set progress=false
 
@@ -15,42 +40,57 @@ case $os in
         if command -v dnf > /dev/null; then
             echo "Updating Node"
 
-            dnf update -y nodejs
-            node -v
-        elif command -v apt-get > /dev/null; then
-            apt-get update
-            apt-get install -y curl tar
+            dnf update -y nodejs > /dev/null 2>&1
 
-            if [[ "$node" < "12.13.0" ]]; then
+            node=$(node -v)
+            node=${node#"v"}
+
+            echo "Node Updated to $node"
+        elif command -v apt-get > /dev/null; then
+            echo "Updating Repositories"
+
+            apt-get update > /dev/null 2>&1
+            apt-get install -y curl tar > /dev/null 2>&1
+
+            if [[ "$node" < "12.13.1" ]]; then
                 case $arch in
                     "x86_64")
-                        echo "Updating Node"
+                        echo "Upgrading Node"
 
-                        curl -O https://nodejs.org/dist/v12.13.0/node-v12.13.0-linux-x64.tar.gz
-                        tar -xzf ./node-v12.13.0-linux-x64.tar.gz -C /usr/local --strip-components=1 --no-same-owner
-                        rm -f ./node-v12.13.0-linux-x64.tar.gz
+                        curl -O https://nodejs.org/dist/v12.13.1/node-v12.13.1-linux-x64.tar.gz > /dev/null 2>&1
+                        tar -xzf ./node-v12.13.1-linux-x64.tar.gz -C /usr/local --strip-components=1 --no-same-owner > /dev/null 2>&1
+                        rm -f ./node-v12.13.1-linux-x64.tar.gz > /dev/null 2>&1
 
-                        node -v
+                        node=$(node -v)
+                        node=${node#"v"}
+
+                        echo "Node Updated to $node"
                         ;;
 
                     "armv7l")
-                        echo "Updating Node"
+                        echo "Upgrading Node"
 
-                        curl -O https://nodejs.org/dist/v12.13.0/node-v12.13.0-linux-armv7l.tar.gz
-                        tar -xzf ./node-v12.13.0-linux-armv7l.tar.gz -C /usr/local --strip-components=1 --no-same-owner
-                        rm -f ./node-v12.13.0-linux-armv7l.tar.gz
+                        curl -O https://nodejs.org/dist/v12.13.1/node-v12.13.1-linux-armv7l.tar.gz > /dev/null 2>&1
+                        tar -xzf ./node-v12.13.1-linux-armv7l.tar.gz -C /usr/local --strip-components=1 --no-same-owner > /dev/null 2>&1
+                        rm -f ./node-v12.13.1-linux-armv7l.tar.gz > /dev/null 2>&1
 
-                        node -v
+                        node=$(node -v)
+                        node=${node#"v"}
+
+                        echo "Node Updated to $node"
                         ;;
 
                     "armv8l")
-                        echo "Updating Node"
+                        echo "Upgrading Node"
 
-                        curl -O https://nodejs.org/dist/v12.13.0/node-v12.13.0-linux-arm64.tar.gz
-                        tar -xzf ./node-v12.13.0-linux-arm64.tar.gz -C /usr/local --strip-components=1 --no-same-owner
-                        rm -f ./node-v12.13.0-linux-arm64.tar.gz
+                        curl -O https://nodejs.org/dist/v12.13.1/node-v12.13.1-linux-arm64.tar.gz > /dev/null 2>&1
+                        tar -xzf ./node-v12.13.1-linux-arm64.tar.gz -C /usr/local --strip-components=1 --no-same-owner > /dev/null 2>&1
+                        rm -f ./node-v12.13.1-linux-arm64.tar.gz > /dev/null 2>&1
 
-                        node -v
+                        node=$(node -v)
+                        node=${node#"v"}
+
+                        echo "Node Updated to $node"
                         ;;
                 esac
             fi
@@ -58,22 +98,43 @@ case $os in
         ;;
 
     "Darwin")
-        if [[ "$node" < "12.13.0" ]]; then
-            echo "Updating Node"
+        if [[ "$node" < "12.13.1" ]]; then
+            echo "Upgrading Node"
 
-            curl -O https://nodejs.org/dist/v12.13.0/node-v12.13.0-darwin-x64.tar.gz
-            tar -xzf ./node-v12.13.0-darwin-x64.tar.gz -C /usr/local --strip-components=1 --no-same-owner
-            rm -f ./node-v12.13.0-darwin-x64.tar.gz
+            curl -O https://nodejs.org/dist/v12.13.1/node-v12.13.1-darwin-x64.tar.gz
+            tar -xzf ./node-v12.13.1-darwin-x64.tar.gz -C /usr/local --strip-components=1 --no-same-owner
+            rm -f ./node-v12.13.1-darwin-x64.tar.gz
 
-            node -v
+            node=$(node -v)
+            node=${node#"v"}
+
+            echo "Node Updated to $node"
         fi
         ;;
 esac
 
+sleep 0.2
+
+echo "Cleaning NPM"
+
+npm cache clean --force > /dev/null 2>&1
+npm install -g npm > /dev/null 2>&1
+
 echo "Installing HOOBS"
 
-npm cache clean --force
-npm install -g npm
+npm install -g --unsafe-perm @hoobs/hoobs > /dev/null 2>&1
 
-npm install -g --unsafe-perm @hoobs/hoobs
+sleep 0.2
+
+kill -9 $marker
+
+echo "Configuring"
+echo "---------------------------------------------------------"
+
+echo ""
+echo ""
+
+echo "Initializing HOOBS"
+echo "---------------------------------------------------------"
+
 hoobs-init
